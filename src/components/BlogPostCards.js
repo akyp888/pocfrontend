c5d619828ae5d333deb05a6b9cee34da876ff113
akyp888 <akashrkashyap98@gmail.com>
2021-08-15 23:42:48 +0530

    minor change

--- src/components/BlogPostCards.js
+++ src/components/BlogPostCards.js
@@ -2,14 +2,10 @@ import BlogPostCard from "./BlogPostCard"
 import axios from 'axios';
 import { useState } from "react";
 
-const getallposts = axios.get("127.0.0.1:8080/api/posts/")
+const getallposts = axios.get("http://127.0.0.1:8080/api/posts/")
 
 const BlogPostCards = () => {
-    const [posts,setPosts] = useState([{
-        id:20,
-        title:"undefined",
-        description:"undefined"
-    }]);
+    const [posts,setPosts] = useState([]);
     getallposts.then(response => setPosts(response.data))
     return(
             <div className="container" style={{width:50+"%",margin:"auto"}}>
