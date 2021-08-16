450e1a972b5f43adf4c27f684fbd35abe5e764d6
akyp888 <akashrkashyap98@gmail.com>
2021-08-16 20:46:56 +0530

    added redux, thunk

--- src/components/BlogPostCard.js
+++ src/components/BlogPostCard.js
@@ -20,7 +20,7 @@ setModalState({show:true,id:post.id,title:post.title,description:post.descriptio
     }
 
     const deletePost = (id) => {  
-        axios.delete(`http://52.206.79.151:8080/api/posts/${id}`).then(console.log(id))
+        axios.delete(`http://52.206.79.151:8080/api/posts/${id}`).then(response => {console.log(response);window.location.reload(false)})
         }
     return(
         
@@ -28,14 +28,16 @@ setModalState({show:true,id:post.id,title:post.title,description:post.descriptio
 <div className="card" style={{maxWidth: 18 + "rem",margin:"auto"}}>
   <div className="card-body">
     <h5 className="card-title">{postdata.title}</h5>
-    <h6 className="card-subtitle mb-2 text-muted">{postdata.id}</h6>
-    <p className="card-text">{postdata.description}</p>
+    <h6 className="card-subtitle mb-2 text-muted "><small>{`id: ${postdata.id}`}</small></h6>
+    <h6 className="card-subtitle text-muted "><small>description:</small></h6>
+    <div class="b">{postdata.description}</div>
+    <p className="card-text"></p>
     <a href="#" className="btn btn-primary" style={{ margin : 3 + "%"}} onClick = {()=>(showPost(postdata))} >view</a>
     <a href="#" className="btn btn-danger" onClick={()=>{return(deletePost(postdata.id))}}>delete</a>
   </div>
 </div>
 <Modal show={modalState.show}>
-     <Modal.Header>{`id: ${modalState.id} ${modalState.title}`}</Modal.Header>
+     <Modal.Header>{` ${modalState.title}`}<small>{`id: ${modalState.id}`}</small></Modal.Header>
      <Modal.Body>{modalState.description}</Modal.Body>
     <Modal.Footer>{<button className="btn btn-primary" onClick={()=>(setModalState({show:false}))}>close</button>}</Modal.Footer>
     </Modal>
